f8d90498600e92e27565dc855ddf5fed886680d2
Victor Martinez <martinez.victor@correo.unimet.edu.ve>
2023-10-28 22:58:32 -0400

    Clase Fuerte

---
+++ src/Clases/Fuerte.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author Victor
+ */
+public class Fuerte {
+    private boolean fuerte;
+    
+    
+    public Fuerte (){
+        this.fuerte = false;
+    }
+    
+    
+}
